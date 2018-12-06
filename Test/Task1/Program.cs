3a6015c6eff182b4bd98df60c9bfa36fec4dc633
ivanduzunov <ivan.d.uzunov@gmail.com>
2018-12-06 17:09:37 +0200

    initial commit

---
+++ Test/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Task1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string pattern = @"#[A-Z][a-z]+[.][A-Z][a-z]+";         
+            string input = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(input, pattern);
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
---
+++ Test/Task2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[,] matrix = new string[3, 3];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var input = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int a = 0; a < matrix.GetLength(1); a++)
+                {
+                    matrix[i, a] = input[a];
+                }
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int a = 0; a < matrix.GetLength(1); a++)
+                {
+                    Console.Write("Neighbour for " + matrix[i, a] + " is:");
+                    if (i > 0)
+                    {
+                        Console.Write(matrix[i - 1, a]);
+                    }
+
+                    if (i < matrix.GetLength(0) - 1)
+                    {
+                        Console.Write(matrix[i + 1, a]);
+                    }
+
+                    if (a > 0)
+                    {
+                        Console.Write(matrix[i, a - 1]);
+                    }
+
+                    if (a < matrix.GetLength(1) - 1)
+                    {
+                        Console.Write(matrix[i, a + 1]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
